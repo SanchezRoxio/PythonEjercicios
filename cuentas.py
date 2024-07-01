7fb7e467f5145fd52c8bcdcd8cdcb0fb68e79a5c
Rocio <sanchezmaribel.ro@gmail.com>
2024-07-01 16:18:54 -0300

    Hice los ejercicios

--- cuentas.py
+++ cuentas.py
@@ -1,4 +1,17 @@
 #Ejemplos de cuentas
-print (6*3)
-print (6/3)
-print (400*5)
+print("Ejercicio 8")
+print ((10+3)*(6+6))
+
+print("Ejercicio 9")
+print (10/2)
+
+print("Ejercicio 10")
+print (10/3)
+
+print("Ejercicio 11")
+variableA = 12
+variableB = 2
+print(variableA+variableB)
+print(variableA-variableB)
+print(variableA*variableB)
+print(variableA/variableB)
