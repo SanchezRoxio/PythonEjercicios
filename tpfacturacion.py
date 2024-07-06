28ca633bab7c5eb6f010270e04455bb2fe6d2c93
Rocio <sanchezmaribel.ro@gmail.com>
2024-07-06 13:41:34 -0300

    Arregle el programa

--- tpfacturacion.py
+++ tpfacturacion.py
@@ -1,17 +1,25 @@
 #A. Ingresar tres precios de productos y mostrar la suma de los mismos.
-#B. Ingresar tres precios de productos y mostrar el promedio de los mismos.
-#C. Ingresar tres precios de productos sumarlos y mostrar el precio final (más IVA 21%).
-
 pre1 = int(input("Ingrese el primer precio del producto: "))
 pre2 = int(input("Ingrese el segundo precio: "))
 pre3 = int(input("ingrese el tercer precio: "))
-
 sum1 = (pre1 + pre2 + pre3)
+
+print("La suma de los tres precios es de: " , sum1)
+
+#B. Ingresar tres precios de productos y mostrar el promedio de los mismos.
+pre1 = int(input("Ingrese el primer precio del producto: "))
+pre2 = int(input("Ingrese el segundo precio: "))
+pre3 = int(input("ingrese el tercer precio: "))
 prom1 = ((pre1 + pre2 + pre3)/3)
+
+print("El promedio de los mismos es de: " , prom1)
+
+#C. Ingresar tres precios de productos sumarlos y mostrar el precio final (más IVA 21%).
+pre1 = int(input("Ingrese el primer precio del producto: "))
+pre2 = int(input("Ingrese el segundo precio: "))
+pre3 = int(input("ingrese el tercer precio: "))
+sum1 = (pre1 + pre2 + pre3)
 pfinal = (((pre1 + pre2 + pre3)*21)/100)
-
-print("La suma de los tres precios es: " , sum1)
-
-print("El promedio de los mismos es: " , prom1)
-
-print("El precio final es de: " , sum1 , "y con el 21% IVA= " , pfinal)
+sum2 = pfinal + sum1 
+print("El precio es de: " , sum1 , "+ el 21% IVA de= " , pfinal)
+print("El precio final es de: " , sum2)
